ec9586e96361393823f32c7aa832f361c5ccb2ee
Matthew Gibbons <mhgibbons@me.com>
2024-10-06 08:21:43 +0000

    Support extensions

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,2 +1,10 @@
 export { default as Mdast } from './components/Mdast.svelte';
 export { default as mdastComponents } from './defaults/mdast-components.js';
+export type {
+	AllExtensions,
+	AllMdastExtensions,
+	ExtensionMap,
+	Extensions,
+	MdastExtensionMap,
+	MdastExtensions
+} from './defaults/mdast.js';
