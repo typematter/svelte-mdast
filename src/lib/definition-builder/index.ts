5fd9b382287a7037f3b2f8de66959a8509913ddc
Matthew Gibbons <mhgibbons@me.com>
2025-10-19 04:41:31 +0000

    Remove definition builder and related files from the project

--- src/lib/definition-builder/index.ts
+++
@@ -1,9 +0,0 @@
-import type { definitionBuilder } from './definition-builder.js';
-
-declare module '@typematter/svelte-unist' {
-	interface UnistContext {
-		getDefinition?: ReturnType<typeof definitionBuilder>;
-	}
-}
-
-export * from './definition-builder.js';
